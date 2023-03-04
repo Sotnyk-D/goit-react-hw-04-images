21cabe9c1dff61565f167ef2f7c55f922d729528
Dmytro <112177564+Sotnyk-D@users.noreply.github.com>
2023-03-04 18:47:32 +0100

    add

---
+++ src/api/Api.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+export const fetchImages = async (query, page) => {
+  const response = await axios.get(`https://pixabay.com/api/`, {
+    method: 'get',
+    params: {
+      key: '33395330-543dfcfc57dd38064198ac425',
+      q: query,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      per_page: 12,
+      page: page,
+    },
+  });
+
+  return response.data;
+};
